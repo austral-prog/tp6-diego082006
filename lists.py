2cee425e7f7d7239155f2e87631996f91b90eeb6
diego082006 <ddelgado1@mail.austral.edu.ar>
2025-11-18 18:39:09 -0300

    Update lists.py

--- lists.py
+++ lists.py
@@ -1,12 +1,8 @@
-# Replace the "ANSWER HERE" with your answer
-
 def remove_elements(list_to_remove_elements):
     result = []
-
     for i in range(len(list_to_remove_elements)):
-        if i not in [0, 4, 5]:
+        if i not in (0, 4, 5):
             result.append(list_to_remove_elements[i])
-
     return result
 
 
@@ -21,13 +17,11 @@ def is_empty(list_to_check):
 def check_lists(list_to_compare1, list_to_compare2):
     if len(list_to_compare1) < 3 or len(list_to_compare2) < 3:
         return False
-
     return list_to_compare1[2] == list_to_compare2[2]
 
 
 def list_of_lists(list_of_lists_to_modify):
-    list1 = list_of_lists_to_modify[0][:2]          
-    list2 = list_of_lists_to_modify[1][1:4]         
-    list3 = list_of_lists_to_modify[2][-2:]         
-
+    list1 = list_of_lists_to_modify[0][:2]
+    list2 = list_of_lists_to_modify[1][1:4]
+    list3 = list_of_lists_to_modify[2][-2:]
     return [list1, list2, list3]
